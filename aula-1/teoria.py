5ad0fc838b9ca408cab27b103370467b0224cca6
Professor <Professor@SN-407007.edu.fiesc.com.br>
2026-04-22 21:52:53 -0300

    meu primeiro código em python

---
+++ aula-1/teoria.py
@@ -0,0 +1,15 @@
+# isso é um comentário
+
+# agora nós escreveremos na tela usando o comando print!
+# o comando print, serve para escrevermos alguma coisa
+
+print("Olá, mundo!")
+
+# o comando input serve para receber uma entrada
+# do teclado, sendo que o usuário irá digitar
+
+# a sintaxe do comando é imput("mensagem")
+nome = input("Digite o seu nome: ")
+# nome é a variável, quando o nome é colocado, fica armazenado
+print("Olá" , nome)
+# , serve para juntar 
